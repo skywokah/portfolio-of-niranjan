11d28d06d01274381282584f6043bba5b993e559
skywokah <sniranjankrishna@gmail.com>
2025-01-19 18:12:06 +0530

    Add missing assets and update package.json

--- src/components/Resume/ResumeNew.jsx
+++ src/components/Resume/ResumeNew.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Niranjan Krishna Web Developer Resume.pdf";
+import pdf from "../../Assets/NiranjanKrishna_Web_Developer_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        // Add the path to the external assets here
+        '@assets/home-main.svg',
+        '@assets/about.png',
+        '@assets/NiranjanKrishna_Web_Developer_Resume.pdf',
+        '@assets/Projects/emotion.png',
+        // Add other external assets as needed
+      ],
+    },
+  },
 })
