8a2ed9438da5a8b5bcb3425f8818d308659db833
skywokah <sniranjankrishna@gmail.com>
2025-01-21 22:28:02 +0530

    Update Node.js version to 18.18.0 and ensure compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Use this to simplify imports
+    },},
   build: {
     rollupOptions: {
       external: [
